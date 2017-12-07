92e133da57845bafdcc2cd2231112435f0f48d2b
3dw1nM0535 <workockmoses@gmail.com>
2017-12-07 18:18:50 +0300

    wrapped activeUser reducer

--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users-reducer';
+import ActiveUserReducer from './active-user-reducer';
 
 export const allReducers = combineReducers({
-  users: usersReducer
+  users: usersReducer,
+  activeUser: ActiveUserReducer
 });
 
