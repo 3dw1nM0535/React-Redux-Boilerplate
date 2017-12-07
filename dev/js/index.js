5ee24fe5c80ebfe6079dff69e15db1720da44800
3dw1nM0535 <workockmoses@gmail.com>
2017-12-07 16:43:09 +0300

    index file for combining all reducers to one big JavaScript Object

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,5 +1,9 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import allReducers from './reducers/index';
+
+const store = createStore(allReducers);
 
 render(<h1>Hey Redux App</h1>, document.getElementById('root'));
