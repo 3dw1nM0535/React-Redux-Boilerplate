c4b8db80d6f15549267af2c63f40163152fe165a
3dw1nM0535 <workockmoses@gmail.com>
2017-12-07 18:18:10 +0300

    listen for user selected action

---
+++ dev/js/reducers/active-user-reducer.js
@@ -0,0 +1,11 @@
+//Listens for actions
+export default function (state=null, action) {
+  switch (action.type) {
+    case 'USER_SELECTED':
+      return action.payload;
+      break;
+  }
+
+  return state;
+}
+
