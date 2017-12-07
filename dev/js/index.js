8c0d72b4bdcecbccf9499333456582bcd049c330
3dw1nM0535 <workockmoses@gmail.com>
2017-12-07 17:02:44 +0300

    wrap component in Provider

--- dev/js/index.js
+++ dev/js/index.js
@@ -2,8 +2,14 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
-import allReducers from './reducers/index';
+import { allReducers } from './reducers/index';
+import App from './components/App';
+import { Provider } from 'react-redux';
 
 const store = createStore(allReducers);
 
-render(<h1>Hey Redux App</h1>, document.getElementById('root'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
